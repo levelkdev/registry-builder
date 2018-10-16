512f20dadd2d5eeb2b76685e6b30235ca7a377af
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-16 17:04:17 -0400

    more tests for BasicRegistry, using lk-test-helpers

--- test/BasicRegistry.test.js
+++ test/BasicRegistry.test.js
@@ -1,9 +1,10 @@
 const web3Utils = require('web3-utils')
+const { expectEvent, shouldFail } = require('lk-test-helpers')(web3)
 
 const BasicRegistry = artifacts.require('BasicRegistry')
 
 contract('BasicRegistry', () => {
-  let basicRegistry, tx
+  let basicRegistry, logs
 
   const itemData = web3Utils.padRight(
     web3Utils.fromAscii('listing 001'),
@@ -18,20 +19,21 @@ contract('BasicRegistry', () => {
   describe('add()', () => {
     describe('when the given data is not in the items mapping', () => {
       beforeEach(async () => {
-        tx = await basicRegistry.add(itemData)
+        logs = (await basicRegistry.add(itemData)).logs
       })
 
       it('adds the data to the items mapping', async () => {
         expect(await basicRegistry.get(itemId)).to.equal(itemData)
       })
 
-      it.skip('emits an ItemAdded event', () => {
-        throw new Error('not implemented')
+      it('emits an ItemAdded event', () => {
+        expectEvent.inLogs(logs, 'ItemAdded', { id: itemId })
       })
     })
     describe('when the given data is in the items mapping', () => {
-      it.skip('reverts', () => {
-        throw new Error('not implemented')
+      it('reverts', async () => {
+        await basicRegistry.add(itemData)
+        await shouldFail.reverting(basicRegistry.add(itemData))
       })
     })
   })
