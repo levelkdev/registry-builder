78d3ba2e84c08f3b487eb333b18dc4c3c88ad6b9
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-16 17:52:26 -0400

    add all tests for BasicRegistry

--- test/BasicRegistry.test.js
+++ test/BasicRegistry.test.js
@@ -1,5 +1,11 @@
 const web3Utils = require('web3-utils')
-const { expectEvent, shouldFail } = require('lk-test-helpers')(web3)
+const {
+  expectEvent,
+  shouldFail,
+  constants
+} = require('lk-test-helpers')(web3)
+
+const { ZERO_BYTES32 } = constants
 
 const BasicRegistry = artifacts.require('BasicRegistry')
 
@@ -17,6 +23,7 @@ contract('BasicRegistry', () => {
   })
 
   describe('add()', () => {
+
     describe('when the given data is not in the items mapping', () => {
       beforeEach(async () => {
         logs = (await basicRegistry.add(itemData)).logs
@@ -30,6 +37,7 @@ contract('BasicRegistry', () => {
         expectEvent.inLogs(logs, 'ItemAdded', { id: itemId })
       })
     })
+  
     describe('when the given data is in the items mapping', () => {
       it('reverts', async () => {
         await basicRegistry.add(itemData)
@@ -37,4 +45,53 @@ contract('BasicRegistry', () => {
       })
     })
   })
+
+  describe('remove()', () => {
+
+    describe('when the given id is in the items mapping', () => {
+      beforeEach(async () => {
+        await basicRegistry.add(itemData)
+        logs = (await basicRegistry.remove(itemId)).logs
+      })
+
+      it('removes the data from the items mapping', async () => {
+        expect(await basicRegistry.get(itemId)).to.equal(ZERO_BYTES32)
+      })
+
+      it('emits an ItemRemoved event', () => {
+        expectEvent.inLogs(logs, 'ItemRemoved', { id: itemId })
+      })
+    })
+
+    describe('when the given id is not in the items mapping', () => {
+      it('reverts', async () => {
+        await shouldFail.reverting(basicRegistry.remove(itemId))
+      })
+    })
+
+  })
+
+  describe('get()', () => {
+    it('returns data for the given id', async () => {
+      await basicRegistry.add(itemData)
+      expect(await basicRegistry.get(itemId)).to.equal(itemData)
+    })
+  })
+
+  describe('exists()', () => {
+
+    describe('when given an id that exists', () => {
+      it('returns true', async () => {
+        await basicRegistry.add(itemData)
+        expect(await basicRegistry.exists(itemId)).to.be.true
+      })
+    })
+
+    describe('when given an id that does not exist', () => {
+      it('returns false', async () => {
+        expect(await basicRegistry.exists(itemId)).to.be.false
+      })
+    })
+  })
+
 })
