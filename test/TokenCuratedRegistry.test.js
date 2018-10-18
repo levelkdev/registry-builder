12c0e7b58c2cf4dc3e03c2f1e3c81ac5e47fe118
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-18 14:43:55 -0400

    add() behavior tests for TokenCuratedRegistry

--- test/TokenCuratedRegistry.test.js
+++ test/TokenCuratedRegistry.test.js
@@ -1,3 +1,4 @@
+const { shouldBehaveLikeTokenCuratedRegistry } = require('./TokenCuratedRegistry.behavior')
 const { shouldBehaveLikeTimelockableItemRegistry } = require('./TimelockableItemRegistry.behavior')
 const { shouldBehaveLikeStakedRegistry } = require('./StakedRegistry.behavior')
 const { shouldBehaveLikeOwnedItemRegistry } = require('./OwnedItemRegistry.behavior')
@@ -11,17 +12,29 @@ contract('TokenCuratedRegistry', function (accounts) {
   const [owner, rando] = accounts
   const initialBalance = 100 * 10 ** 18
   const minStake = 10 * 10 ** 18
+  const applicationPeriod = 60 * 60
   const challengeFactoryAddress = '0x9497F19985AE5e02F7A0dEc7FeE521eaE678d0F7'
 
   beforeEach(async function () {
     this.now = (await web3.eth.getBlock('latest')).timestamp
     this.token = await TestToken.new(
-      'TestToken',
-      'TEST',
-      18,
       [owner, rando],
       initialBalance
     )
+  })
+
+  describe('when application period is greater than 0', function () {
+    beforeEach(async function () {
+      this.registry = await TokenCuratedRegistry.new(
+        this.token.address,
+        minStake,
+        applicationPeriod,
+        challengeFactoryAddress
+      )
+      await this.token.approve(this.registry.address, minStake, { from: owner })
+    })
+
+    shouldBehaveLikeTokenCuratedRegistry(minStake, initialBalance, applicationPeriod, accounts)
   })
 
   describe('when application period is 0', function () {
