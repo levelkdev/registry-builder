104886bc6a44367f71ca530bfacf0b0edc2b1ee2
Emily Williams <emily.williams@levelk.io>
2018-10-18 12:58:36 -0400

    Add contract code comments

--- contracts/Challenge/PLCRVotingChallenge/PLCRVotingChallenge.sol
+++ contracts/Challenge/PLCRVotingChallenge/PLCRVotingChallenge.sol
@@ -6,25 +6,43 @@ import '../IChallenge.sol';
 import '../../Registry/TokenCuratedRegistry.sol';
 import 'openzeppelin-zos/contracts/token/ERC20/ERC20.sol';
 
+/**
+ * PLCRVotingChallenge is a registry challenge that creates a poll on a
+ * PLCRVoting Contract where voters decide whether to keep or accept an
+ * item onto the registry
+ *
+ * This Challenge contract interfaces with PLCRVoting, communicates results to the
+ * registry, and distributes voter rewards
+**/
 contract PLCRVotingChallenge is IChallenge {
   using SafeMath for uint;
 
   event ChallengeClosed();
   event RewardClaimed(uint reward, address voterAddress);
 
-  address public challenger;     // address of the challenger
-  address public listingOwner;   // address of the listingOwner
-  uint public challengerStake;   // amount of tokens challenger staked in challenge
-  TokenCuratedRegistry public registry;     // address of registry
-  PLCRVoting public voting;      // address of PLCRVoting Contract
-  uint public pollID;             // poll ID for PLCRVoting contract
-  uint public rewardPool;        // pool of tokens to be distributed to winning voters
-  bool public isClosed;          // signifies whether challenge has been closed
-  uint public voterTokensClaimed;
-  uint public voterRewardsClaimed;
+  address public challenger;             // address of the challenger
+  address public listingOwner;           // address of the listingOwner
+  uint public challengerStake;           // amount of tokens challenger staked in challenge
+  TokenCuratedRegistry public registry;  // address of registry
+  PLCRVoting public voting;              // address of PLCRVoting Contract
+  uint public pollID;                    // poll ID for PLCRVoting contract
+  uint public rewardPool;                // pool of tokens to be distributed to winning voters
+  bool public isClosed;                  // signifies whether challenge has been closed
+  uint public voterTokensClaimed;        // total amount of winning tokens voters have received rewards for thus far
+  uint public voterRewardsClaimed;       // total amount of rewards distributed to voters thus far
 
-  mapping(address => bool) public tokenClaims;   // Indicates whether a voter has claimed a reward yet
+  mapping(address => bool) public tokenClaims;   // Indicates whether a voter has claimed their reward yet
 
+  // @dev create new challenge
+  // @param _challenger           account that challenged registry item
+  // @param _listingOwner         account of listingOwner
+  // @param _challengerStake      amount challenger staked for challenge
+  // @param _registry             registry contract
+  // @param _plcrVoting           PLCRVoting contract with voting poll
+  // @param _commitStageLength    commitStageLength for PLCRVoting poll
+  // @param _revealStageLength    revealStageLength for PLCRVoting poll
+  // @param _voteQuorum           voteQuorum needed for winning outcome
+  // @param _percentVoterReward   percent of challengerStake rewarded to voters
   constructor (
     address _challenger,
     address _listingOwner,
@@ -70,10 +88,13 @@ contract PLCRVotingChallenge is IChallenge {
       return !voting.isPassed(pollID);
   }
 
+
+
   // =========================
   // WINNER REWARD INTERFACE:
   // =========================
 
+  // @dev     returns the total reward amount to be distributed to challenge winner
   function reward() public view returns (uint rewardAmount) {
     require(isClosed);
 
@@ -84,6 +105,8 @@ contract PLCRVotingChallenge is IChallenge {
       rewardAmount = challengerStake * 2 - rewardPool;
     }
   }
+
+
 
   // =========================
   // VOTER REWARD INTERFACE:
@@ -119,6 +142,7 @@ contract PLCRVotingChallenge is IChallenge {
       return (voterTokens * remainingRewardPool) / remainingTotalTokens;
   }
 
+  // @dev returns challenger address
   function challenger() view returns (address) {
     return challenger;
   }
