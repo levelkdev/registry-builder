00b0d9704ddd13b6d6f623d3dd93ae52ca59756f
Mike Calvanese <mfcalvanese@gmail.com>
2018-10-26 02:32:31 -0400

    modify truffle config to include all testnets

---
+++ truffle.js
@@ -0,0 +1,41 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+// first read in the secrets.json to get our mnemonic
+let secrets;
+let mnemonic;
+if (fs.existsSync('secrets.json')) {
+  secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+  mnemonic = secrets.mnemonic;
+} else {
+  console.log('no secrets.json found. You can only deploy to the testrpc.');
+  mnemonic = '';
+}
+
+module.exports = {
+  networks: {
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      network_id: '*',
+      gas: 6000000,
+      gasPrice: 20 * 10 ** 9,
+    },
+    ropsten: {
+      provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io'),
+      network_id: '*',
+      gas: 6000000,
+      gasPrice: 20 * 10 ** 9,
+    },
+    kovan: {
+      provider: new HDWalletProvider(mnemonic, 'https://kovan.infura.io'),
+      network_id: '*',
+      gas: 6000000,
+      gasPrice: 20 * 10 ** 9,
+    },
+    development: {
+      host: 'localhost',
+      port: 8545,
+      network_id: '*',
+    }
+  },
+};
